9a2228b1735282721bec7d13ebaed38719a56da2
DeathEnder1 <113164349+DeathEnder1@users.noreply.github.com>
2024-04-04 13:50:31 +0700

    first commit

---
+++ movies/movies/src/test/java/com/buncua/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buncua.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
