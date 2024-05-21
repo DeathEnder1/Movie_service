a46434862f4e855ea968f729f2445312e699d927
MorpheusOQug <qahp955@gmail.com>
2024-05-22 03:20:49 +0700

    update pop up trailer ?

--- movies/movives_frontend/src/components/MovieData.jsx
+++ movies/movives_frontend/src/components/MovieData.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './MovieData.css'
 
-function MovieData() {
+function MovieData({movie}) {
   return (
-    <div className="date">
-        <h2>On 15th August</h2>
+    <div className={`date ${movie.active ? 'active': undefined}`}>
+        <h2>On {movie.date}</h2>
     </div>
   );
 }
