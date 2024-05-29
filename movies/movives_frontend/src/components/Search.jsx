f0632a0c7a42ee3a9b962260816bfdcf01824b24
DeathEnder1 <113164349+DeathEnder1@users.noreply.github.com>
2024-05-29 11:25:17 +0700

    commit

--- movies/movives_frontend/src/components/Search.jsx
+++ movies/movives_frontend/src/components/Search.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+// import React from 'react'
+
+// import './Search.css';
+
+// function Search() {
+//   return (
+//     <div className='Search'>
+//         <input type='text' placeholder='Search'></input> <ion-icon name="search-outline"></ion-icon>
+//     </div>
+//   )
+// }
+
+// export default Search
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './Search.css';
 
-function Search() {
-  return (
-    <div className='Search'>
-        <input type='text' placeholder='Search'></input> <ion-icon name="search-outline"></ion-icon>
-    </div>
-  )
-}
+const Search = () => {
+    const [keyword, setKeyword] = useState('');
+    const navigate = useNavigate();
 
-export default Search
+    const handleSearch = () => {
+        navigate(`/search/${keyword}`);
+    };
+
+    return (
+      <div className='Search'>
+        <input
+            type="text"
+            placeholder="Search for movies"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                    handleSearch();
+                }
+            }}
+        />
+        <ion-icon name="search-outline"></ion-icon>
+      </div>
+        
+    );
+};
+
+export default Search;
