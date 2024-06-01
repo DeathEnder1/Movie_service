9ae2cd8351489293fa95f2f881a1c91fe9d43a6c
DeathEnder1 <113164349+DeathEnder1@users.noreply.github.com>
2024-06-01 18:04:30 +0700

    added admin frontend

--- movies/movives_frontend/src/components/Card.jsx
+++ movies/movives_frontend/src/components/Card.jsx
@@ -11,6 +11,7 @@
           <div className="col-lg-2 col-md-4 col-sm-6">
               <div className="movie-card">
                   <img src={movie.previewimg} alt="Preview_Image" className='img-fluid' />
+                  <p style={{ paddingTop: '2vh' }}>{movie.title}</p>
                   <p>{movie.length} | {movie.category}</p>
                   <div className="content">
                       <h4>{movie.title}</h4>
